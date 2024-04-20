adf86ba90eb6a94cf32258764de7e7c65a06f592
Jeremy Gottlieb <jeremy@gottlieb.cyou>
2024-04-21 00:59:36 +0200

    fix build.sh

--- src/build.sh
+++ src/build.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname $0)
 #cd /home/jj/Projects/snake/src
-g++ -c snake.cpp -o .snake.o -std=c++17
-g++ .snake.o graphic.cpp -o main -lsfml-graphics -lsfml-window -lsfml-system -std=c++17
+g++ -c snake.cpp -o .snake.o -std=c++17 -ggdb -O0
+g++ .snake.o graphic.cpp -o main -lsfml-graphics -lsfml-window -lsfml-system -std=c++17 -ggdb
